fca71db5f69ff012c997bb8eb40f2c6543657cd0
raj1122 <arialstarraj@gmail.com>
2019-12-22 19:13:10 +0530

    First Changes

---
+++ src/test/java/com/example/cipherchatzy/CipherchatzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cipherchatzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipherchatzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
